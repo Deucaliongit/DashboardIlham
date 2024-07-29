6997ff8da64f525f75d8951be80d7473cefa743a
Deucaliongit <ilhamrifai166@gmail.com>
2024-07-29 16:25:51 +0700

    logs

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -9,6 +9,7 @@ import { MdComputer } from "react-icons/md";
 import { IoMdClose } from "react-icons/io";
 import { FaComputer } from "react-icons/fa6";
 import Logs from "../miniComponents/Logs";
+
 const Main = () => {
   return (
     <div className="flex flex-col md:flex-row bg-slate-100 dark:bg-slate-950 ">
@@ -39,17 +40,25 @@ const Main = () => {
           <Card icon={<IoMdClose />} ket={"Total Asset Rusak"} jumlah={"15"} />
         </div>
       </section>
-      <section className="w-full md:w-[25%] bg-white dark:bg-slate-900/30">
-        <div className="w-auto m-4 dark:bg-slate-900/90 p-4 rounded-md flex flex-col gap-2">
-          <div className="dark:text-slate-400 font-semibold text-md md:text-lg pb-2">
-            Ilham Rifai
+      <section className="w-full md:w-[25%] dark:bg-slate-900/30 border-l">
+        <div className="w-auto m-4 dark:bg-slate-900/90 bg-white p-4 rounded-md flex flex-col gap-2.5">
+          <div className="dark:text-slate-400 font-semibold text-md md:text-lg flex justify-between items-center">
+            <div>Ilham Rifai</div>
+            <div className="hidden md:flex">
+              <img className="w-8 h-8 rounded-full" src={profile} />
+            </div>
           </div>
           <div className="dark:text-slate-400 font-semibold text-xs md:text-md pb-2">
             Administrator
           </div>
-          <div className="text-indigo-900 font-bold text-sm md:text-md dark:text-transparent dark:bg-gradient-to-tl from-indigo-800 to-pink-800 py-2 rounded-md cursor-pointer ">
-            <div className="dark:text-slate-400 text-slate-900 text-center">
+          <div className="text-indigo-900 font-bold text-sm md:text-md dark:text-transparent dark:bg-gradient-to-tl from-indigo-800 to-pink-800 py-2 rounded-md cursor-pointer bg-indigo-800 ">
+            <div className="dark:text-slate-400 text-white text-center">
               Profile
+            </div>
+          </div>
+          <div className="text-indigo-900 font-bold text-sm md:text-md dark:text-transparent dark:bg-gradient-to-tl from-indigo-800 to-pink-800 py-2 rounded-md cursor-pointer bg-slate-600 ">
+            <div className="dark:text-slate-400 text-white text-center">
+              Logout
             </div>
           </div>
         </div>
--- src/components/miniComponents/Logs.jsx
+++ src/components/miniComponents/Logs.jsx
@@ -3,13 +3,13 @@ import { MdRefresh } from "react-icons/md";
 
 const Logs = ({ tanggal, action }) => {
   return (
-    <div className="w-auto text-slate-900 dark:text-slate-400 dark:bg-slate-900/90 p-4 rounded-md flex justify-start items-center gap-2 text-sm font-semibold">
-      <div className="text-lg md:text-2xl bg-slate-950 p-1.5 rounded-full">
+    <div className="w-auto bg-white text-slate-900 dark:text-slate-400 dark:bg-slate-900/90 p-4 rounded-md flex justify-start items-center gap-2 text-sm font-semibold">
+      <div className="text-lg md:text-2xl bg-slate-100 p-1.5 rounded-full">
         <MdRefresh />
       </div>
       <div className="flex flex-col text-sm">
         <div className="">{action}</div>
-        <div className="text-indigo-600 dark:text-indigo-800">{tanggal}</div>
+        <div className="text-indigo-800 dark:text-indigo-800">{tanggal}</div>
       </div>
     </div>
   );
